b3a17fd27870f16b3c6b16dafa00778f62689c9c
Michael Pacheco <michaelpacheco.br@gmail.com>
2020-09-14 18:25:49 -0300

    Criando a classe Posicao

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace _12_XadrezMichael
 {
@@ -6,7 +7,14 @@ namespace _12_XadrezMichael
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
         }
     }
 }
